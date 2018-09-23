7da757d3942a66d1185e3c156d4f60fb4fac695d
Ilya <ilyaryabchinski@gmail.com>
2018-09-24 00:18:11 +0300

    fixes

---
+++ src/app/core/user.service.spec.ts
@@ -0,0 +1,47 @@
+import {UserService} from './user.service';
+import {UserModel} from '../shared/models/user.model';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(() => {
+    service = new UserService();
+    const store = {};
+    const mockSessionStorage = {
+
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      }
+    };
+    spyOn(sessionStorage, 'getItem')
+      .and.callFake(mockSessionStorage.getItem);
+    spyOn(sessionStorage, 'setItem')
+      .and.callFake(mockSessionStorage.setItem);
+    spyOn(sessionStorage, 'removeItem')
+      .and.callFake(mockSessionStorage.removeItem);
+  });
+
+  it('should log in', () => {
+    service.logIn({name: 'ilyar'});
+    expect(service.isLogggedIn()).toBeTruthy();
+  });
+
+  it('should log out', () => {
+    service.logIn({name: 'ilyar'});
+    service.logOut();
+    expect(service.isLogggedIn()).toBeFalsy();
+  });
+
+  it('should return user', () => {
+    const user: UserModel = {name: 'ilyar'};
+    service.logIn(user);
+    expect(service.getLoggedUser()).toBe(user);
+  });
+
+});
